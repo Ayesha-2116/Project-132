41d44c0a7bda39a6b047e6fd4eeb9b51d65e61ad
Ayesha-2116 <ayesha.2023.siddiqua@gmail.com>
2024-11-13 11:33:41 -0500

    Initial Commit

---
+++ Intermediate/Build/Win64/x64/P132VideoChatEditor/Development/Slate/SharedPCH.Slate.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33141
+#include "C:/Users/User/Documents/Unreal Projects/P132VideoChat/Intermediate/Build/Win64/x64/P132VideoChatEditor/Development/Slate/SharedPCH.Slate.Cpp20.h"
---
+++ Intermediate/Build/Win64/x64/P132VideoChatEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/User/Documents/Unreal Projects/P132VideoChat/Intermediate/Build/Win64/x64/P132VideoChatEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
